5dc4d6b09aa65198150bb992cae1f2f7112e48a5
T0tally <140659684+T0tally@users.noreply.github.com>
2023-09-04 22:29:09 -0700

    Update DiscordSelectFix.js

--- DiscordSelectFix.js
+++ DiscordSelectFix.js
@@ -1,7 +1,7 @@
 /**
  * @name DiscordSelectFix
- * @author T0tally
- * @description There is a bug in Discord that prevents users from selecting text in certain situations, such as after pressing the "esc" key, toggling the quickswitcher, or enlarging a photo in a channel where the users don't have permission to write. "EscNoSelectionFix" has been created to solve this issue. To use it, simply press the "esc" key again and the selection functionality will be restored.
+ * @author _totally_
+ * @description There is a bug in Discord that prevents users from selecting text in certain situations, such as after pressing the "esc" key, toggling the quickswitcher, or enlarging a photo in a channel where the users don't have permission to write. "EscNoSelectionFix" has been created to solve this issue. To use it, simply press the "esc" key and the selection functionality will be restored.
  * @version 0.0.1
  */
 
